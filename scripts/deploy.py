d284a0dc4b97ac2bc0cefc0821a46f3268857f8a
Nguyễn Khắc Trường <johnkt12qz@gmail.com>
2024-07-01 02:40:19 +0700

    ci: Deploy contract not relying on truffle

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -3,36 +3,26 @@ import os
 from dotenv import load_dotenv
 from solcx import compile_source
 from web3 import Web3
+from requests.exceptions import ConnectionError
 load_dotenv()
-# Connect to Ganache
 web3 = Web3(Web3.HTTPProvider(os.getenv("BLOCKCHAIN_ADDRESS")))
 
-# Check if connected
 if web3.is_connected():
     print("Connected to Ethereum node")
 else:
-    print("Failed to connect")
+    raise ConnectionError("Failed to connect")
 
-# Set default account
-web3.eth.default_account = web3.eth.accounts[0]
-with open("../contracts/Election.sol", "r") as contract_file:
-    contract_source_code = contract_file.read()
+with open("contracts/Election.sol", "r") as contract_file:
+    contract_source_code = compile_source(contract_file.read())
+    contract_id, contract_interface = contract_source_code.popitem()
 
-    compiled_sol = compile_source(source=contract_source_code)
-    contract_interface = compiled_sol["<stdin>:Election"]
-
-    # Get the bytecode and ABI
-    bytecode = contract_interface["bin"]
+    bytecode = contract_interface["bytecode"]
     abi = contract_interface["abi"]
 
-    # Set up the account
-    account = web3.eth.account.from_key(os.getenv("PRIVATE_KEY"))
+    w3 = Web3(Web3.EthereumTesterProvider())
+    w3.eth.default_account = w3.eth.accounts[0]
+    Election = w3.eth.contract(abi=abi, bytecode=bytecode)
 
-    # Deploy contract
-    Election = web3.eth.contract(abi=abi, bytecode=bytecode)
-    tx_hash = Election.constructor().transact(
-        {"nonce": web3.eth.get_transaction_count(account)}
-    )
-    tx_receipt = web3.eth.wait_for_transaction_receipt(tx_hash)
+    tx_hash = Election.constructor().transact()
 
-    print(f"Contract deployed at address: {tx_receipt}")
+    tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
